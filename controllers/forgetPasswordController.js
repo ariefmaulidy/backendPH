6af7f823592eec74e977e183c066cc4fc539eee2
nac17 <nugrohoac17@gmail.com>
2017-03-26 18:59:00 +0700

    fix pswd 2 2

--- controllers/forgetPasswordController.js
+++ controllers/forgetPasswordController.js
@@ -13,13 +13,14 @@ var forgetPassword = function(req,res ){
 		charset: 'alphabetic'
 	});
 
-	var email 	="";
+	//var email 	="";
 
-	User.findOne({username:req.body.username},function (err,user){
-		user.password=crypto.createHash('md5').update(newPassword, 'ut-8').digest('hex');
-		console.log(user.email);
-		var email 	= user.email;
-		user.save(function(err){
+User.findOne({username:req.body.username},function (err,user){
+	user.password=crypto.createHash('md5').update(newPassword, 'ut-8').digest('hex');
+	console.log(user.password);
+	console.log(newPassword);
+	var email 	= user.email;
+	user.save(function(err){
 			if(!err){
 				res.status(200).json({status:200,message:'Update success'});
 			}
@@ -28,31 +29,36 @@ var forgetPassword = function(req,res ){
 				//res.status(400).json({status:400,message:'bad request'});
 				console.log('filed update password');
 			}
+
+			var mailOptions = {
+				from: '"PORTAL-HARGA" <portalharga.ipb@gmail.com>',
+				to: email,
+				subject: 'Forget Password',
+				html:
+				'Saudara/i '+ req.body.username + ' password baru anda : ' + newPassword + '<br>'+
+				'Setelah berhasil login segera ubah password anda <br>' +
+				'ini password yang di database : ' + user.password + '<br>' +
+				'ini password yang baru yang tidak di encrip : ' + newPassword
+				//'baris 2'+
+			};
+
+			transporter.sendMail(mailOptions, function(error, info){
+				if(error){
+					return console.log(error);
+				}
+				else {
+					console.log('Message sent: ' + info.response);
+					res.json({
+						message:"succes",
+						email:email
+					});
+				}
+			});
+
 		});
 	});
 
-	var mailOptions = {
-		from: '"PORTAL-HARGA" <portalharga.ipb@gmail.com>',
-		to: email,
-		subject: 'Forget Password',
-		html:
-		'Saudara/i '+ req.body.username + ' password baru anda : ' + newPassword + '<br>'+
-		'Setelah berhasil login segera ubah password anda'
-		//'baris 2'+
-	};
 
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
-			return console.log(error);
-		}
-		else {
-			console.log('Message sent: ' + info.response);
-			res.json({
-				message:"succes",
-				email:email
-			});
-		}
-	});
 }
 
 module.exports = {
