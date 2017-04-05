5c032a1d76b170d1257b67aa890f574c84da8250
Ryan Baskara <ryan.baskara@gmail.com>
2017-04-05 21:22:42 +0700

    upload foto dagangan petani

--- controllers/daganganController.js
+++ controllers/daganganController.js
@@ -7,19 +7,31 @@ var fs 			=	require('fs');
 var fromNow 	= 	require('from-now');
 var tz 			=	require('moment-timezone');
 var ImageSaver 	=	require('image-saver-nodejs/lib');
+var os = require("os");
 
 var getDaganganKu = function(req,res){
-	Dagangan.find({user_id:req.params.id},'-_id -__v',{sort:{datePost:-1}}).lean().exec(function(err,dagangan){
+	Dagangan.find({user_id:req.params.dagangan_id},'-_id -__v',{sort:{datePost:-1}}).lean().exec(function(err,dagangan){
 					if(dagangan!='')
 							{ var counter = 0;
 									each(dagangan,function(value,key,array){
-										User.findOne({user_id:dagangan[key].user_id}).exec(function(err,user){
-										dagangan[key].time=fromNow(dagangan[key].datePost);
-										counter++;
-										if(counter==dagangan.length)
-											{									
-							 					res.json({status:200,message:'Get data success',data:dagangan,token:req.token});		
-											}
+										User.findOne({user_id:dagangan[key].user_id}).exec(function(err,user)
+										{
+										//	console.log('ini user id = '+dagangan[key].user_id);
+
+											dagangan[key].picture=req.get('X-Forwarded-Protocol')+'/'+dagangan[key].picture;
+											dagangan[key].nama=user.name;
+											dagangan[key].address=user.address;
+											dagangan[key].time=fromNow(dagangan[key].datePost);
+											Komoditas.findOne({komoditas_id:dagangan[key].komoditas_id}).exec(function(err,komoditas)
+											{
+												dagangan[key].nama_komoditas=komoditas.name;
+												dagangan[key].satuan_komoditas=komoditas.satuan;
+												counter++;
+												if(counter==dagangan.length)
+													{									
+									 					res.json({status:200,message:'Get data success',data:dagangan,token:req.token});		
+													}
+											});
 										});
 									})			
 								}
@@ -36,14 +48,15 @@ var getAll = function(req,res){
 									each(dagangan,function(value,key,array){
 										User.findOne({user_id:dagangan[key].user_id}).exec(function(err,user)
 										{
-											console.log('ini user id = '+dagangan[key].user_id);
+										//	console.log('ini user id = '+dagangan[key].user_id);
+											dagangan[key].picture='https://ph.yippytech.com'+'/'+dagangan[key].picture;
 											dagangan[key].nama=user.name;
-											dagangan[key].picture=user.picture;
 											dagangan[key].address=user.address;
 											dagangan[key].time=fromNow(dagangan[key].datePost);
 											Komoditas.findOne({komoditas_id:dagangan[key].komoditas_id}).exec(function(err,komoditas)
 											{
 												dagangan[key].nama_komoditas=komoditas.name;
+												dagangan[key].satuan_komoditas=komoditas.satuan;
 												counter++;
 												if(counter==dagangan.length)
 													{									
@@ -51,8 +64,6 @@ var getAll = function(req,res){
 													}
 											});
 										});
-										
-										
 									})			
 								}
 				else
@@ -63,11 +74,12 @@ var getAll = function(req,res){
 }
 var postDagangan = function(req,res){
 		dagangan = new Dagangan(req.body);
+		dagangan.user_id = req.user_id;
 		var imageSaver = new ImageSaver();
 	  	var time=moment();
-	  	if(req.body.string64!=null){
+	  	if(req.body.picture!=null){
 	  		dagangan.picture="uploads/foto_komoditas/"+req.user_id+"_"+req.body.komoditas_id+"_"+Date.parse(moment(time).tz('Asia/Jakarta'))+".jpg";	
-				imageSaver.saveFile("../public_html/"+dagangan.picture, req.body.string64)
+				imageSaver.saveFile("../public_html/"+dagangan.picture, req.body.picture)
 					.then((data)=>{
 						console.log("upload photo success"); 
 			    		})
@@ -99,23 +111,26 @@ var updateDagangan = function(req,res){
 		if(dagangan!=null){
 			if(dagangan.user_id==req.user_id){
 				var time=moment();
+				var imageSaver = new ImageSaver();
 				dagangan.komoditas_id 		=	req.body.komoditas_id;
 				dagangan.harga				=	req.body.harga;
-				dagangan.stok				= 	req.body.stok;
+				dagangan.keterangan			=	req.body.keterangan;
 				dagangan.picture			= 	dagangan.picture;
+				dagangan.stok				= 	req.body.stok;
 				dagangan.datePost 			= 	Date.parse(moment(time).tz('Asia/Jakarta')); 
-				if(req.body.string64!=null){
-					if(dagangan.picture!=null){
+				if(req.body.picture!=null){
+					if(dagangan.picture!=null)
+					{
 						fs.unlinkSync('../public_html/'+dagangan.picture);
-						dagangan.picture="uploads/foto_komoditas/"+req.user_id+"_"+req.body.komoditas+"_"+moment(time).tz('Asia/Jakarta')+".jpg";	
-						imageSaver.saveFile("../public_html/"+dagangan.picture, req.body.string64)
-						.then((data)=>{
-							console.log("upload photo success"); 
-				    		})
-			    		.catch((err)=>{
-							res.json({status:400,message:err,token:req.token});
-							})
-						}
+					}	
+					dagangan.picture="uploads/foto_komoditas/"+req.user_id+"_"+req.body.komoditas+"_"+moment(time).tz('Asia/Jakarta')+".jpg";	
+					imageSaver.saveFile("../public_html/"+dagangan.picture, req.body.picture)
+					.then((data)=>{
+						console.log("upload photo success"); 
+			    		})
+		    		.catch((err)=>{
+						res.json({status:400,message:err,token:req.token});
+						})	
 		  			}
 		  		dagangan.save(function(err)
 				{
@@ -145,21 +160,32 @@ var updateDagangan = function(req,res){
 var delDagangan = function(req,res){
 	Dagangan.findOne({dagangan_id:req.body.dagangan_id},function(err,dagangan){
 	//	res.json({aspirasi});
-		if(dagangan!=null)
-		{
-			if(dagangan.picture!=null && dagangan.user_id==req.user_id){
+	//	console.log(dagangan);
+		if(dagangan!=null) 
+		{ 
+			if(dagangan.user_id==req.user_id || req.role==1)
+			{
+				if(dagangan.picture!=null && dagangan.user_id==req.user_id)
+				{
 					fs.unlinkSync('../public_html/'+dagangan.picture);
+				}
+				dagangan.remove(function(err){
+					if(!err)
+					{
+						res.status(200).json({status:200,message:"delete success",token:req.token});
 					}
-			dagangan.remove(function(err){
-				if(dagangan.user_id==req.user_id){
-					res.status(200).json({status:200,message:"delete success",token:req.token});
-				}
-				else{
-					res.status(403).json({status:403,message:"Forbidden",token:req.token});
-				}
-			})
+					else
+					{
+						res.status(403).json({status:403,message:err,token:req.token});
+					}
+				})
+			}
+			else
+			{
+				res.status(403).json({status:403,message:"Forbidden",token:req.token});
+			}
 		}
-		else
+		else if(dagangan==null)
 		{
 			res.status(204).json({status:204,message:"Not Found",token:req.token});
 		}
