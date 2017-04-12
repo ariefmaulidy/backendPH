f731810902138dec0642526e132f5cb50572b9c3
Ryan Baskara <ryan.baskara@gmail.com>
2017-04-12 12:07:25 +0700

    user not found

--- routes/auth.js
+++ routes/auth.js
@@ -7,66 +7,39 @@ var fs        =   require('fs');
 
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 authRoutes.post('', function(req, res) {
-  //generate password hash
-  generated_hash = require('crypto')
-  .createHash('md5')
-  .update(req.body.password+'portalharga', 'utf8')
-  .digest('hex');
-  // find the user
-  User.findOne({username: req.body.username}, function(err, user) {
-      if (err) throw err;
-      if (user=='') 
-      {
-          res.json({ status:500,success: false, message: 'Authentication failed. User not found.' });
-      } 
-      else if(user) 
-      {
-          // check if password matches
-          if (user.password != generated_hash) 
+  if(req.body.login_type==null)
+  {
+      res.json({ status:500,success: false, message: 'Please send login_type' });
+  }
+  else
+  {
+      //generate password hash
+      generated_hash = require('crypto')
+      .createHash('md5')
+      .update(req.body.password+'portalharga', 'utf8')
+      .digest('hex');
+      // find the user
+      User.findOne({username: req.body.username}, function(err, user) {
+          if (user==null) 
           {
-              res.json({ status:500,success: false, message: 'Authentication failed. Wrong password.' });
+              res.json({ status:500,success: false, message: 'Authentication failed. User not found.' });
           } 
-          else 
+          else if(user) 
           {
-              var dt = new Date();
-              var utcDate = dt.toGMTString();
-              user.password=generated_hash;
-              user.last_login=utcDate;
-              user.save(function(err){
-              if(err)
+              // chec if password matches
+              if (user.password != generated_hash) 
               {
-                  var saveData = '';
-                  res.json({success:false,status:"500",message: "Add Failed"});
-              }
-              else
+                  res.json({ status:500,success: false, message: 'Authentication failed. Wrong password.' });
+              } 
+              else 
               {
-                  // for website
-                  if(req.body.login_type==0)
+                  var dt = new Date();
+                  var utcDate = dt.toGMTString();
+                  user.last_login=utcDate;
+                  user.save(function(err)
                   {
-                    var token = jwt.sign({
-                                            user_id:user.user_id,
-                                            username:user.username,
-                                            time:user.last_login,
-                                            role:user.role,
-                                            login_type:req.body.login_type
-                                          }
-                                          ,codeSecret.secret, {
-                                          expiresIn : 60*60// expires in 24 hours
-                                          });
-                        User.findOne({username: req.body.username}, '-_id -__v -password',function(err, result)
-                        {
-                            if (err) 
-                            {
-                                return console.log(err);
-                            }
-                            else 
-                            {
-                                res.json({success: true,status:200,message: 'Login Success',data : result,token: token});    
-                            }
-                        });
-                    }
-                    // for mobile
-                    else if(req.body.login_type==1)
+                    // for website
+                    if(req.body.login_type==0)
                     {
                       var token = jwt.sign({
                                               user_id:user.user_id,
@@ -75,29 +48,43 @@ authRoutes.post('', function(req, res) {
                                               role:user.role,
                                               login_type:req.body.login_type
                                             }
-                                            ,codeSecret.secret,{
-                                            //no expires
+                                            ,codeSecret.secret, {
+                                            expiresIn : 60*60// expires in 24 hours
                                             });
+                      }
+                      // for mobile
+                      else if(req.body.login_type==1)
+                      {
+                        var token = jwt.sign({
+                                                user_id:user.user_id,
+                                                username:user.username,
+                                                time:user.last_login,
+                                                role:user.role,
+                                                login_type:req.body.login_type
+                                              }
+                                              ,codeSecret.secret,{
+                                              //no expires
+                                              });
+                      }
+                      setTimeout(function()
+                      {
                           User.findOne({username: req.body.username}, '-_id -__v -password',function(err, result)
                           {
                               if (err) 
                               {
-                                return console.log(err);
+                                  return console.log(err);
                               }
                               else 
                               {
-                                res.json({success: true,status:200,message: 'Login Success',data : result,token: token});    
+                                  res.json({success: true,status:200,message: 'Login Success',data : result,token: token});    
                               }
-                          });
-                    }
-              }
-          });
-    
-      }   
-
-    }
-
-  });
+                          });        
+                      },100);
+                  });
+               }   
+            }
+        });
+     }
 });
 
 module.exports=authRoutes; 
