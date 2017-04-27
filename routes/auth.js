49a38dcb672e2e1556625165a4de3950529b9f47
nugrohoac <nugrohoac17@gmail.com>
2017-04-27 19:19:12 +0700

    fix function

--- routes/auth.js
+++ routes/auth.js
@@ -79,7 +79,7 @@ authRoutes.post('', function(req, res) {
                               }
                               else 
                               {
-                                  mail.getValidate(req,res,user.isValidate,user.email,user.username,user.name);
+                                  mail.getValidate(req,res,user.isValidate,user.email,user.username,user.name,user.user_id);
                                   res.json({success: true,status:200,message: 'Login Success',data : result,token: token});    
                               }
                           });        
