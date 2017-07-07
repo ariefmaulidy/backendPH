2a5eec7daed93c4dfef269afd93d21e1a4358c61
nugrohoac <nugrohoac17@gmail.com>
2017-07-07 17:21:52 +0700

    weekly

--- controllers/masyarakat/laporanHargaController.js
+++ controllers/masyarakat/laporanHargaController.js
@@ -280,7 +280,7 @@ var monthLaporan = function(req,res){
 			//tanggal sekarang
 			var dateNow = new Date();
 			//bulan
-			var getDate = dateFormat(dateNow, "mmmm, yyyy");						
+			var monthDate = dateFormat(dateNow, "mmmm, yyyy");						
 			//console.log(getDate);
 			//buat variabel parsing yang akan menerima laporanHarga_id pada hari itu
 			var parsing = [];
@@ -288,7 +288,7 @@ var monthLaporan = function(req,res){
 			var counter = 0;
 				
 			for(var i=0;i<all.length;i++){
-				if(dateFormat(all[i].datePost, "mmmm, yyyy")==getDate){
+				if(dateFormat(all[i].datePost, "mmmm, yyyy")==monthDate){
 					number.push(all[i].laporanHarga_id);					
 				};
 			}
@@ -312,7 +312,57 @@ var monthLaporan = function(req,res){
 			setTimeout(function () {
 				res.json({	
 					status:200,
-					message:"sukses mendapat laporan harga " + getDate +"",
+					message:"sukses mendapat laporan harga " + monthDate +"",
+					data:parsing,						
+					token:req.token
+				});
+			}, 400);
+		}
+	})
+}
+
+var weekLaporan = function(req,res){
+	//ambil semua laporanHarga di sorting sesuai dengan tanggal post
+	laporanHarga.find({},'-_id -__v',{sort:{datePost:-1}},function(err,all){
+		if(all==null){
+			res.json({status:204,message:err,data:"",token:req.token});
+		}else{
+			//tanggal sekarang
+			var dateNow = new Date();
+			//bulan
+			var weekDate = dateFormat(dateNow, "W");						
+			//console.log(getDate);
+			//buat variabel parsing yang akan menerima laporanHarga_id pada hari itu
+			var parsing = [];
+			var number = [];
+			var counter = 0;
+				
+			for(var i=0;i<all.length;i++){
+				if(dateFormat(all[i].datePost, "W")==weekDate){
+					number.push(all[i].laporanHarga_id);					
+				};
+			}
+			//time out 300 miliseconds
+			setTimeout(function () {
+				for(var i=0;i<number.length;i++){
+					laporanHarga.findOne({laporanHarga_id:number[i]},'-_id -__v').lean().exec(function(err,laporan){
+						komoditas.findOne({komoditas_id:laporan.komoditas_id}).exec(function(err,komo){
+							user.findOne({user_id:laporan.user_id},function(err,masyarakat){
+								laporan.namaKomoditas=komo.name;
+								laporan.satuan = komo.satuan;
+								laporan.nama = masyarakat.name;
+								laporan.datePost = moment(laporan.datePost).format("YYYY-MM-DD");;
+								parsing.push(laporan);
+							})
+						})						
+					})					
+				}
+			}, 300);
+			//time out 400 mili seconds
+			setTimeout(function () {
+				res.json({	
+					status:200,
+					message:"sukses mendapat laporan harga pada minggu ke " + weekDate +"",
 					data:parsing,						
 					token:req.token
 				});
@@ -330,6 +380,7 @@ module.exports = {
 	delete:deleteLaporan,
 	getDay:dayLaporan,
 	laporanku:laporanHargaKu,
-	getMonth:monthLaporan
+	getMonth:monthLaporan,
+	getWeek:weekLaporan
 
 }
